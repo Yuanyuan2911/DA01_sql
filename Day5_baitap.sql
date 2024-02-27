64d3804c9f55ec65d6a59c2e68bfc0e7f625b9a4
Yuanyuan2911 <161067600+Yuanyuan2911@users.noreply.github.com>
2024-02-27 22:33:34 +0700

    Update Day5_baitap.sql

--- Day5_baitap.sql
+++ Day5_baitap.sql
@@ -1,1 +1,47 @@
 
+---ex5
+SELECT candidate_id
+FROM candidates
+WHERE skill IN('Python','Tableau','PostgreSQL')
+GROUP BY CANDIDATE_ID
+HAVING COUNT(skill)=3;
+---ex6
+SELECT user_id,
+DATE(max(post_date))-DATE(min(post_date)) as days_between
+FROM posts
+WHERE post_date>='2021-02-02' AND post_date<'2022-01-01'
+GROUP BY user_id
+HAVING COUNT(post_id)>=2
+---ex7
+SELECT card_name,
+MAX(issued_amount)- MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC
+---ex8
+SELECT manufacturer,
+COUNT(drug) AS drug_count,
+ABS(SUM(cogs-total_sales)) AS total_loss
+FROM pharmacy_sales
+WHERE total_sales<cogs
+GROUP BY manufacturer	
+ORDER BY total_loss DESC
+---ex9
+SELECT * FROM Cinema
+WHERE id%2=1 AND description !='boring'
+ORDER BY rating DESC
+---ex10
+SELECT teacher_id,
+COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id
+---ex11
+SELECT user_id, 
+COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
+---ex12
+SELECT class FROM Courses
+GROUP BY class
+HAVING COUNT(student )>=5
