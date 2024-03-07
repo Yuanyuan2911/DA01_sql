3def393737580b77b2d3b87f27bfc659c8829eb0
Yuanyuan2911 <161067600+Yuanyuan2911@users.noreply.github.com>
2024-03-07 23:13:06 +0700

    Update Day9_baitap.sql

--- Day9_baitap.sql
+++ Day9_baitap.sql
@@ -4,3 +4,22 @@ COUNT('laptop') as laptop_reviews,
 COUNT('phone') + COUNT('tablet') as mobile_views
 FROM viewership
 ---ex02
+
+---ex03
+
+---ex04
+SELECT NAME FROM customer
+WHERE referee_id IS NULL OR referee_id=1;
+---ex05
+---(CASE 
+---  WHEN pclass = '1' THEN ' first_class'
+---  ELSE '0'
+---END),
+--(CASE 
+---  WHEN pclass = '2' THEN ' second_classs'
+---  ELSE '0'
+---END),
+--- (CASE 
+---  WHEN pclass = '3' THEN ' third_class'
+---  ELSE '0'
+---END)
