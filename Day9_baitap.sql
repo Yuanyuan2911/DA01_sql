b9a163a4c3541a3f31184dcbdfcc2be4db303e3d
Yuanyuan2911 <161067600+Yuanyuan2911@users.noreply.github.com>
2024-03-09 20:15:32 +0700

    Update Day9_baitap.sql

--- Day9_baitap.sql
+++ Day9_baitap.sql
@@ -4,22 +4,21 @@ COUNT('laptop') as laptop_reviews,
 COUNT('phone') + COUNT('tablet') as mobile_views
 FROM viewership
 ---ex02
-
+SELECT x,y,z,
+CASE
+WHEN x + y > z and  x + z > y and  y + z > x THEN 'Yes'
+ELSE 'No'
+END as triangle
+FROM Triangle
 ---ex03
 
 ---ex04
 SELECT NAME FROM customer
 WHERE referee_id IS NULL OR referee_id=1;
 ---ex05
----(CASE 
----  WHEN pclass = '1' THEN ' first_class'
----  ELSE '0'
----END),
---(CASE 
----  WHEN pclass = '2' THEN ' second_classs'
----  ELSE '0'
----END),
---- (CASE 
----  WHEN pclass = '3' THEN ' third_class'
----  ELSE '0'
----END)
+SELECT survived,
+    sum(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) AS first_class,
+    sum(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) AS second_class,
+    sum(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) AS third_class
+FROM titanic
+GROUP BY  survived
